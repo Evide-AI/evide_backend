e2debe210046b2f4bf41b7c860e62947952a8482
Aakamsh PM <aakamshpm@gmail.com>
2025-09-15 22:21:59 +0530

    style: format db.js

--- config/db.js
+++ config/db.js
@@ -1,31 +1,31 @@
-import { Sequelize } from 'sequelize'
+import { Sequelize } from "sequelize";
 
 const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: 'postgres',
-        logging: console.log,
-        pool: {
-            max: 5,
-            min: 0,
-            acquire: 30000,
-            idle: 10000
-        }
-    }
-)
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "postgres",
+    logging: console.log,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  }
+);
 
-export const connectDB = async ()=>{
-    try{
-        await sequelize.authenticate();
-        console.log('PostgreSQL connected successfully');
-    }catch(error){
-        console.error('PostgreSQL connection failed:', error.message);
-        throw error;
-    }
+export const connectDB = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("PostgreSQL connected successfully");
+  } catch (error) {
+    console.error("PostgreSQL connection failed:", error.message);
+    throw error;
+  }
 };
 
-export {sequelize}
+export { sequelize };
